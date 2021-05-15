793a51f9f2928800e2f51af2670b78efd11de775
Manuel Stoeckl <code@mstoeckl.com>
2021-05-15 12:44:31 -0400

    Create/destroy a new shm buffer per frame
    Caching these actually increased memory usage after startup;
    compositors like Sway tend to release the buffer on receipt (since
    they have already copied the shm buffer to an OpenGL equivalent)
    so the shm buffer is no longer needed after being used. Outputs
    (when not in nested mode) are generally only configured/drawn once,
    so there is no point in caching data for the future.

--- include/pool-buffer.h
+++ include/pool-buffer.h
@@ -9,14 +9,12 @@ struct pool_buffer {
 	struct wl_buffer *buffer;
 	cairo_surface_t *surface;
 	cairo_t *cairo;
-	uint32_t width, height;
 	void *data;
 	size_t size;
-	bool busy;
 };
 
-struct pool_buffer *get_next_buffer(struct wl_shm *shm,
-		struct pool_buffer pool[static 2], uint32_t width, uint32_t height);
+bool create_buffer(struct pool_buffer *buffer, struct wl_shm *shm,
+		int32_t width, int32_t height, uint32_t format);
 void destroy_buffer(struct pool_buffer *buffer);
 
 #endif
