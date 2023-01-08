2c76e5e69fe5e92e2e8329a5ea14a8e5eb82d709
Manuel Stoeckl <code@mstoeckl.com>
2023-01-07 23:27:16 -0500

    Remove unused function

--- include/cairo_util.h
+++ include/cairo_util.h
@@ -9,10 +9,6 @@
 #endif
 
 void cairo_set_source_u32(cairo_t *cairo, uint32_t color);
-cairo_subpixel_order_t to_cairo_subpixel_order(enum wl_output_subpixel subpixel);
-
-cairo_surface_t *cairo_image_surface_scale(cairo_surface_t *image,
-		int width, int height);
 
 #if HAVE_GDK_PIXBUF
 
