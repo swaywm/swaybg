918c7ab11a9897f6f197af9fcf4d009ae76036e1
Manuel Stoeckl <code@mstoeckl.com>
2024-10-20 12:22:09 -0400

    Render SVG files at output resolution
    This adds an optional dependency on librsvg to directly render the
    SVG images, instead of using gdk-pixbuf to draw them to an
    intermediate image.

--- include/background-image.h
+++ include/background-image.h
@@ -1,6 +1,11 @@
 #ifndef _SWAY_BACKGROUND_IMAGE_H
 #define _SWAY_BACKGROUND_IMAGE_H
 #include "cairo_util.h"
+#if HAVE_RSVG
+#include "librsvg/rsvg.h"
+#else
+typedef struct _RsvgHandle RsvgHandle;
+#endif
 
 enum background_mode {
 	BACKGROUND_MODE_STRETCH,
@@ -16,5 +21,9 @@ enum background_mode parse_background_mode(const char *mode);
 cairo_surface_t *load_background_image(const char *path);
 void render_background_image(cairo_t *cairo, cairo_surface_t *image,
 		enum background_mode mode, int buffer_width, int buffer_height);
+#if HAVE_RSVG
+void render_background_image_svg(cairo_t *cairo, RsvgHandle *svg,
+				 enum background_mode mode, int buffer_width, int buffer_height);
+#endif
 
 #endif
